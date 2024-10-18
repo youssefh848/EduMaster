a8c0eaacbc613389457a2222403bea297b2a901d
Youssef <youssefhossam205@gmail.com>
2024-10-18 17:01:08 +0300

    add db

---
+++ db/models/lessonModel.js
@@ -0,0 +1,4 @@
+// schema
+
+
+// model 
--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// hi im youssef 
+import path from 'path'
+import express from 'express'
+import dotenv from "dotenv";
+import { dbConnection } from './db/connection.js'
+const app = express()
+const port = process.env.PORT || 3000;
+dotenv.config({ path: path.resolve("./config/.env") })
+// db connection
+dbConnection()
+
+
+app.listen(port, () => console.log(`app listening on port ${port}!`))
