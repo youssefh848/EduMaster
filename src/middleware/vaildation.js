95f06fd2c85b1919e81b27f709e497d9af6a149d
mahmoud <mhmwdbhjt307@gmail.com>
2024-10-19 00:32:10 +0300

    conflict 1

--- src/middleware/vaildation.js
+++ src/middleware/vaildation.js
@@ -1,7 +1,7 @@
 // import modules
 import joi from 'joi';
 import { AppError } from '../utils/appError.js';
-import { highSchool } from '../utils/constant/enums.js';
+import { highSchool } from '../utils/constant/enums.js'; // Resolved import from main
 
 export const generalFields = {
     fullName: joi.string(),
@@ -9,13 +9,15 @@ export const generalFields = {
     password: joi.string().pattern(new RegExp(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)),
     cpassword: joi.string().valid(joi.ref('password')),
     phoneNumber: joi.string().pattern(new RegExp(/^01[0-2,5]{1}[0-9]{8}$/)),
-    classLevel: joi.string().valid(...Object.values(highSchool)),
+    classLevel: joi.string().valid(...Object.values(highSchool)),  // Kept from main
     DOB: joi.string()
         .regex(/^\d{4}-(0?[1-9]|1[0-2])-(0?[1-9]|[12][0-9]|3[01])$/)
         .message('Date of birth must be in format YYYY-M-D or YYYY-MM-DD'),
     objectId: joi.string().hex().length(24),
     otp: joi.string().length(6),
-
+    title: joi.string(),  // Kept from Youssef
+    description: joi.string().min(10).max(1000),  // Kept from Youssef
+    video: joi.string().pattern(new RegExp(/^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$/)),  // Kept from Youssef
 }
 
 export const isValid = (schema) => {
